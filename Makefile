fa31bd2211493b74859717bb3ec6382dbf9e3b18
Kat Lilly <kat.lilly@gmail.com>
2018-01-19 13:53:52 +1300

    still wrestling with CMake and Xcode

---
+++ Makefile
@@ -0,0 +1,36 @@
+
+CC = gcc
+CFLAGS = -ansi -pedantic -W -Wall -g
+
+
+pme: pme.c
+	$(CC) $(CFLAGS) -o pme pme.c
+
+runpme: pme postings.bin
+	./pme postings.bin
+
+clean:
+	rm -f pme
+
+
+
+#notsimple9: permutations.c notsimple9.c
+#	$(CC) $(CFLAGS) -o notsimple9 notsimple9.c permutations.c -lm
+
+
+#compare-notsimple9: notsimple9 postings.bin
+#	./notsimple9 postings.bin
+
+
+#compress: compress.c
+#	$(CC) $(CFLAGS) -c compress.c
+
+
+#wsj-compress: compress.c wsj-compress.c
+#	$(CC) $(CFLAGS) -o wsj-compress wsj-compress.c compress.c
+
+
+#do-wsj-compress: wsj-compress postings.bin
+#	./wsj-compress postings.bin
+
+
--- pme.c
+++ pme.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <math.h>
+
+#define NUMBER_OF_DOCS (1024 * 1024 * 128)
+
+static uint32_t *postings_list;
 
 
-int main(void) {
-    printf("hello\n");
+int main(int argc, char *argv[])
+{
+    printf("entered main\n");
+    int listnumber = 0;
+    
+    const char *filename;
+    
+    if (argc == 2) {
+        filename = argv[1];
+    } else {
+        exit(printf("Usage::%s <binfile>\n", argv[0]));
+    }
+    
+    postings_list = malloc(NUMBER_OF_DOCS * sizeof postings_list[0]);
+    
+    FILE *fp;
+    if ((fp = fopen(filename, "rb")) == NULL) {
+        exit(printf("Cannot open %s\n", filename));
+    }
+    
+    uint32_t length;
+    while (fread(&length, sizeof(length), 1, fp)  == 1) {
+        
+        /* Read one postings list (and make sure we did so successfully) */
+        if (fread(postings_list, sizeof(*postings_list), length, fp) != length) {
+            exit(printf("i/o error\n"));
+        }
+        
+        listnumber++;
+        
+        
+    }/* end read-in of a single list*/
+    
+    printf("number of lists: %d\n", listnumber);
+    
     return 0;
 }
+
