2e5f13eeb0cbed44174968bcd744d2027f9893ec
kat <kat.lilly@gmail.com>
2019-06-06 13:29:15 +1200

    add basic CI

--- regressiontests.sh
+++ regressiontests.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-g++ listStats.cpp regtest.cpp -o rtests
+g++ -O3 -std=c++11 listStats.cpp regtest.cpp -o rtests
 ./rtests > mean_stdev.txt
-#diff mean_stdev.txt correct_mean_stdev.txt
-
-
+diff mean_stdev.txt correct_mean_stdev.txt
