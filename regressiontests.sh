82d929cb6cd698ee6747a7051f992c122d9731b9
kat <kat.lilly@gmail.com>
2019-09-06 13:44:03 +1200

    regression tests

--- regressiontests.sh
+++ regressiontests.sh
@@ -7,6 +7,7 @@ diff test_stats.txt correct_stats.txt
 #rm test_sg
 #rm test_sg.txt
 g++ -std=c++11 listStats.cpp selectorGen.cpp sg_test.cpp -o test_sg
-./test_sg sg_testdata.bin > test_sg.txt
-diff test_sg.txt correct_selector_gen.txt
+./test_sg sg_testdata.bin
+#> test_sg.txt
+#diff test_sg.txt correct_selector_gen.txt
 
